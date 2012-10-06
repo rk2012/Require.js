ed64f74fa9bb6733cbd075b25c57911c5efa50e9
raksuns <raksuns@gmail.com>
2012-10-07 01:08:26 +0900

    Add User List Examples

---
+++ examples/main.js
@@ -0,0 +1,9 @@
+require(['User', 'Router'], function(User, Router){
+	var users = [new User('Barney'),
+	             new User('Cartman'),
+	             new User('Sheldon')];
+	
+	localStorage.users = JSON.stringify(users);
+
+	Router.startRouting();
+});
